d65964a95e1cbc09c117377a04b6aa8941e9cbb6
HINA KHADIM <hinakhadim2002@gmail.com>
2020-02-25 23:41:15 +0500

    sol

---
+++ 01_Day/Exercise_sol/variables.js
@@ -0,0 +1,24 @@
+let name="Hina",
+    check=true,
+    car=undefined,
+    bucket=null;
+
+console.log(typeof(name));
+console.log(typeof(check));
+console.log(typeof(car));
+console.log(typeof(bucket));
+
+let var1,var2,var3,var4;
+let variable1=1,variable2=2,variable3=3,variable4=4;
+
+let first_name="Hina";
+let last_name="Khadim";
+let martial_status="Single";
+let country='Pakistan';
+let age=18;
+
+let myAge=18;
+let yourAge=20;
+
+console.log("I am ",myAge," years old.");
+console.log("You are ",yourAge," years old.")
