730ffabcad708aab6f71ca29f25fe0592a6391db
HINA KHADIM <hinakhadim2002@gmail.com>
2020-03-04 16:04:21 +0500

    sol_7_level_3

--- 07_Day/Solution/sol.js
+++ 07_Day/Solution/sol.js
@@ -96,8 +96,200 @@ function macAddress(){
 function hexNumber(){
   return '#'+(Math.random()*0xFFFFFF<<0).toString(16);
 }
-function randomId(){
-    Math.random().toString(36).substring(2, 9);
+function randomId(nchar){
+    Math.random().toString(nchar*2).substring(2, nchar);
 }
 
 //--------------------------LEVEL 3-------------------------//
+
+var nchar=prompt("Number of Character: ");
+var nIds=prompt("Number of Ids: ");
+function userIdgeneratedByUser(nchar=nchar,nIds=nIds){
+    for (var i=0;i<=nIds.length;i++){
+        randomId(nchar);
+    }
+}
+userIdgeneratedByUser();
+
+function rgbColorGenerator(){
+    return (`rgb(${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)})`);
+}
+
+function arrayOfHexaColor(){
+    var array=[];
+    for (var i=0;i<=Math.floor(Math.random()*20);i++){
+        array.push(hexNumber());
+    }
+    return array;
+}
+
+function rgbToHex(r,g,b){
+    const red=r.toString(16);
+    const green=g.toString(16);
+    const blue=b.toString(16);
+
+    if(red.length==1){red="0"+red}
+
+    if(green.length==1){green="0"+green}
+
+    if(blue.length==1){blue="0"+blue}
+}   
+
+function hexToRGB(h) {
+    let r = 0, g = 0, b = 0;
+  console.log(h.length)
+    // 3 digits
+    if (h.length == 4) {
+      r = "0x" + h[1] + h[1];
+      g = "0x" + h[2] + h[2];
+      b = "0x" + h[3] + h[3];
+  
+    // 6 digits
+    } else if (h.length == 7) {
+      r = "0x" + h[1] + h[2];
+      g = "0x" + h[3] + h[4];
+      b = "0x" + h[5] + h[6];
+    }
+    
+    return "rgb("+ +r + "," + +g + "," + +b + ")";
+  }
+
+  function generateColors(format,number){
+      var colors=[];
+      if (format=="hexa"){
+          for (var i=0;i<number;i++){
+              colors.push(hexNumber());
+          }
+      }
+      else{
+        for (var i=0;i<number;i++){
+            colors.push(rgbColorGenerator());
+        }
+      }
+ }
+
+function shuffleArray(arr){
+    var currentIndex=arr.length,tempValue,randIndex;
+    while (0!==currentIndex){
+        randIndex=Math.floor(Math.random()*currentIndex);
+        currentIndex-=1;
+    temp=array=arr[currentIndex];
+    arr[currentIndex]=arr[randIndex];
+    arr[randIndex]=temp;
+
+    }
+}
+
+function factorial(number){
+    var fact=1;
+    for (var i=1;i<=number;i++){
+    fact=fact*i}
+    return number;
+}
+
+function isEmpty(arr){
+    if (arr.length==0){
+        return true;
+    }
+    return false;
+}
+function sum(arguments){
+    var s=0;
+    for (var i=0;i<arguments.length;i++){
+        s=s+arguments[i];
+    }
+    return s;
+}
+
+function sumOfArrayItems(arr){
+    var check=arr.every((item)=> typeof(item)=="number");
+    if (check==true){
+        var s=sum(arr);
+    }
+    else{
+        return "Not a Nubmer";
+    }
+    return s; 
+}
+
+function average(arr){
+    if (typeof(sumOfArrayItems(arr))=="number"){
+        var avg=sumOfArrayItems(arr)/arr.length;
+    }
+    else{
+        console.log("Not a number");
+    }
+    return avg;
+}
+
+function modifyArray(arr){
+    if (arr.length<5){
+        return "Not found";
+    }
+    else{
+        arr[4]=arr[4].toUpperCase();
+        return arr;
+    }
+}
+
+function isPrime(number){
+    if (number <2){return false;}
+    else if (number==2){
+        return true;
+    }
+    else{
+        var count=0;
+        var limit=Math.floor(Math.sqrt(number));
+        for (var i=2;i<=limit;i++){
+            if (number%i==0){
+                count+=1;
+            }
+        }
+        if (count==0){ return true;}
+        else{
+            return false;
+        }
+    }
+}
+
+function unique(arr){
+    return arr.every((num,index)=> arr.indexof(num)==index);
+}
+
+function same_data_type(arr){
+    return arr.every((item)=> typeof(item)===typeof(arr[0]));
+}
+
+function isValidVariable(variable){
+    try{
+        Function(variable+name);
+    }
+    catch(e){
+        return false;}
+    return true;
+}
+
+function randomNumber(){
+    return function(){
+    var arr=[];
+    for (var i=0;i<7;i++)
+    {arr.push(Math.floor(Math.random()*10));}
+    return arr;}
+}
+randomNumber()()
+
+const countries=['Albania',
+'Bolivia',
+'Canada',
+'Denmark',
+'Ethiopia',
+'Finland',
+'Germany',
+'Hungary',
+'Ireland',
+'Japan',
+'Kenya'];
+function reverseCountries(arr){
+    var array=arr;
+    return arr.reverse();
+}
