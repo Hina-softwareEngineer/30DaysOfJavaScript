86333a69f2752f8facc2d8751ded1b6bcee02773
HINA KHADIM <hinakhadim2002@gmail.com>
2020-03-06 17:33:36 +0500

    complete

--- 07_Day/Solution/sol.js
+++ 07_Day/Solution/sol.js
@@ -293,3 +293,136 @@ function reverseCountries(arr){
     var array=arr;
     return arr.reverse();
 }
+
+//-----------------------LEVEL 3----------------------//
+
+const personAccount={
+    firstName:"",
+    lastName:"",
+    incomes:0,
+    expenses:"",
+    description:0,
+    totalincome=function(){return this.incomes+this.description;},
+    totalExpense=function(){return this.expenses+this.description},
+    accountInfo=function(){},
+    addIncome=function(){},
+    accountBalance=function(){},
+};
+
+const users = [
+    {
+        _id: 'ab12ex',
+        username: 'Alex',
+        email: 'alex@alex.com',
+        password: '123123',
+        createdAt:'08/01/2020 9:00 AM',
+        isLoggedIn: false
+    },
+    {
+        _id: 'fg12cy',
+        username: 'Asab',
+        email: 'asab@asab.com',
+        password: '123456',
+        createdAt:'08/01/2020 9:30 AM',
+        isLoggedIn: true
+    },
+    {
+        _id: 'zwf8md',
+        username: 'Brook',
+        email: 'brook@brook.com',
+        password: '123111',
+        createdAt:'08/01/2020 9:45 AM',
+        isLoggedIn: true
+    },
+    {
+        _id: 'eefamr',
+        username: 'Martha',
+        email: 'martha@martha.com',
+        password: '123222',
+        createdAt:'08/01/2020 9:50 AM',
+        isLoggedIn: false
+    },
+    {
+        _id: 'ghderc',
+        username: 'Thomas',
+        email: 'thomas@thomas.com',
+        password: '123333',
+        createdAt:'08/01/2020 10:00 AM',
+        isLoggedIn: false
+    }
+    ];
+
+    const products = [
+  {
+    _id: 'eedfcf',
+    name: 'mobile phone',
+    description: 'Huawei Honor',
+    price: 200,
+    ratings: [
+      { userId: 'fg12cy', rate: 5 },
+      { userId: 'zwf8md', rate: 4.5 }
+    ],
+    likes: []
+  },
+  {
+    _id: 'aegfal',
+    name: 'Laptop',
+    description: 'MacPro: System Darwin',
+    price: 2500,
+    ratings: [],
+    likes: ['fg12cy']
+  },
+  {
+    _id: 'hedfcg',
+    name: 'TV',
+    description: 'Smart TV:Procaster',
+    price: 400,
+    ratings: [{ userId: 'fg12cy', rate: 5 }],
+    likes: ['fg12cy']
+  }
+]
+
+function SignUp(user){
+    var count=0;
+    Object.values(users).map(function(name){
+        if (name.username==user){
+            console.log("User has already an account.");
+        count=1;}
+    });
+    if (count==0){
+        users.push({username:user});
+        console.log("You added successfully.");
+    }
+}
+
+function SignIn(user){
+    var count=0;
+    Object.values(users).map(function(name){
+        if (name.username==user){
+            console.log("User is signed in .");
+        count=1;}
+    });
+    if (count==0){
+        console.log("Sign UP first.");
+    }
+}
+
+function rateProduct(key,rating){
+    products[1].ratings=[{userId:key,rate:rating}];
+}
+
+function averageRating(){
+    let sum=0,count=0;
+    for (let i=0;i<products.length;i++){
+    var total_ratings=products[i].ratings.forEach(function(item){
+        sum+=item.rate;
+        count+=1;
+    });
+}
+    return sum/count;
+}
+
+function LikeProduct(user){
+    products[i].likes.push(user);
+}
+
