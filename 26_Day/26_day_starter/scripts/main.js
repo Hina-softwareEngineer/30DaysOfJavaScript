79357bc5e3b3e6507436f6882ef3177cd5956995
HINA KHADIM <hinakhadim2002@gmail.com>
2020-03-20 21:29:37 +0500

    error resolve

--- 26_Day/26_day_starter/scripts/main.js
+++ 26_Day/26_day_starter/scripts/main.js
@@ -2,10 +2,15 @@ let content=document.querySelector(".content");
 
 let totalCountries=document.createElement("h4");
 totalCountries.textContent="Total Number of Countries: "+countries.length+"";
+totalCountries.style.color="white";
+totalCountries.style.fontWeight="700";
 
 content.appendChild(totalCountries);
 
 let countriesSearch=document.createElement("p");
+countriesSearch.style.color="white";
+countriesSearch.style.fontSize="16px";
+countriesSearch.style.margin="0px";
 
 content.appendChild(countriesSearch);
 
@@ -34,7 +39,6 @@ div.style.width="100%";
 let input=document.createElement("input");
 input.style.width="400px";
 input.placeholder="Search country..."
-input.style.textAlign="center";
 
 div.appendChild(input);
 let i=document.createElement("i");
@@ -44,23 +48,71 @@ content.appendChild(div);
 
 let countryDiv=document.createElement("div");
 countryDiv.className="country";
-countryDiv.style.maxWidth="1100px";
-for (i=0;i<countries.length;i++){
-    let countryName=document.createElement("div");
-    
-    countryName.innerHTML="<section>"+countries[i]+"</section>";
-    countryName.style.textAlign='center';
-    countryName.style.width="150px";
-    countryName.style.height="150px";
-    countryName.style.margin="5px";
-    countryName.style.marginLeft="15px";
-    countryName.style.backgroundColor="red";
-    countryName.style.display="inline-block";
 
-    countryDiv.appendChild(countryName);
+
+data(countries);
+
+
+button1.addEventListener("click",function(){
+    button1.className="active";
+    button2.className="";
+    button3.className="";
+});
+
+input.addEventListener('input',function(){
+    if (button1.className=="active"){
+    if (input.value==""){
+        data(countries);
 }
+    else{
+        let array=countries.filter((country)=> country.startsWith(input.value));
+        countriesSearch.textContent="Countries containing "+input.value+" are "+array.length;
+        data(array);
+    }
+}
+if (button2.className=="active"){
 
-document.body.appendChild(countryDiv);
+    if (input.vaue==""){
+        data(countries);
+    }
+    else{
+        let array=countries.filter((country)=> {
+            if (country.includes(input.value)){
+                return country;
+        }
+    });countriesSearch.textContent="Countries containing "+input.value+" are "+array.length;
+        data(array);
+}}
+
+});
+button2.addEventListener("click",function(){
+    button1.className="";
+    button2.className="active";
+    button3.className="";
+});
+
+button3.addEventListener("click",function(){
+    button1.className="";
+    button2.className="";
+    button3.className="active";
+    data(countries.reverse());    
+});
 
 
+
+function data(num){
+    countryDiv.innerHTML="";
+    for (i=0;i<num.length;i++){
+        let countryName=document.createElement("div");
+        if (num[i]===undefined){
+    
+        }else{
+        countryName.className="column";
+        countryName.innerHTML="<div style='height:inherit;border-radius: 5px; background-color:rgba(0,0,255 ,0.2)'></div><p style='margin-top:-120px;  color:white; font-weight:600; text-transform:uppercase;'>"+num[i]+"</p>"; 
+        countryName.style.textAlign='center';
+        countryDiv.appendChild(countryName);}
+    }
+    
+    document.body.appendChild(countryDiv);
+}
 
