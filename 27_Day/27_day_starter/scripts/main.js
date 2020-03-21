dd2d89df00b463b533c7c2c3aeca8ecb86e25436
HINA KHADIM <hinakhadim2002@gmail.com>
2020-03-21 21:28:41 +0500

    one function not working

---
+++ 27_Day/27_day_starter/scripts/main.js
@@ -0,0 +1,59 @@
+let marque=document.querySelector("h4");
+
+function randomcolor(){
+    return "rgb("+Math.floor(Math.random()*256)+" ,"+Math.floor(Math.random()*256)+", "+Math.floor(Math.random()*256)+" )";
+}
+
+let techs=[
+    ["&#x1f4bb;","Programmer"],
+    ["&#128293;","Motivator"],
+    ["&#128293;","Motivational speaker"],
+    ["&#128214;","Content Creator"],
+    ["&#127793;","Educator"],
+    ["&#128212;","Instructor"],
+];
+
+let lang=['PYTHON','HTML','JAVASCRIPT','REACT','REDUX','NODE','MONGODB','NUMPY','PANDAS'];
+
+
+let spans=document.querySelector(".spans");
+let tech=document.querySelector(".techs");
+
+
+// for (var k=0;k<techs.length;k++){
+//     spans.innerHTML=techs[k][0];
+//     tech.textContent=techs[k][1];
+    
+//     setTimeout(function(){
+//         console.log("hia");
+//     },10000);
+// }
+
+let i=0;
+setInterval(function(){
+    
+    let length1=techs.length;
+    spans.innerHTML=techs[i][0];
+    tech.textContent=techs[i][1];
+    i=i+1;
+
+    
+    if (i==length1){
+        i=0;
+    }    
+},1500);
+
+
+let python=document.querySelector(".tech");
+
+let j=0;
+setInterval(function(){
+    let length2=lang.length;
+    python.textContent=lang[j];
+    python.style.color=randomcolor();
+    j=j+1;
+
+    if (j==length2){
+        j=0;
+    }
+},1000);
